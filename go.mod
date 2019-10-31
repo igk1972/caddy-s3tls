fb0249dd19a55acee6bc6ef4d9c7c2f20a61c776
Igor Kuznetsov <igk@igk.ru>
2019-10-31 16:12:25 +0300

    Fix and updated

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/igk1972/caddy-s3tls
 go 1.12
 
 require (
+	github.com/aws/aws-sdk-go v1.22.2
 	github.com/caddyserver/caddy v1.0.1
 	github.com/mholt/certmagic v0.6.2-0.20190624175158-6a42ef9fe8c2
-  github.com/securityclippy/magicstorage v0.0.2-0.20190809175112-aa82c561cf4a
-  github.com/aws/aws-sdk-go v1.22.2
+	github.com/igk1972/magicstorage v0.0.2-0.20190809175112-aa82c561cf4a
 )
--- storages3.go
+++ storages3.go
@@ -1,26 +1,23 @@
 package storages3
 
 import (
-	"fmt"
-	"net"
 	"os"
-	"path"
-	"strings"
-	"sync"
-	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/caddyserver/caddy/caddytls"
+	"github.com/igk1972/magicstorage"
 	"github.com/mholt/certmagic"
-	"github.com/securityclippy/magicstorage"
 )
 
 const (
-	EnvNameEndpoint = "AWS_ENDPOINT"
-	EnvNameBucket   = "AWS_BUCKET"
-	EnvNamePath     = "AWS_PATH"
-	EnvValueBucket  = "caddy"
-	EnvValuePath    = "certmagic"
+	envNameEndpoint = "AWS_S3_ENDPOINT"
+	envNameRegion   = "AWS_REGION"
+	envNameBucket   = "AWS_BUCKET"
+	envNamePath     = "AWS_PATH"
+	envValueBucket  = "caddy"
+	envValuePath    = "certmagic"
 )
 
 func init() {
@@ -28,33 +25,33 @@ func init() {
 }
 
 func constructS3ClusterPlugin() (certmagic.Storage, error) {
-	return magicstorage.NewS3Storage()
+	return newS3Storage(), nil
 }
 
-func NewS3Storage() *S3Storage {
+func newS3Storage() *magicstorage.S3Storage {
 	cfg := aws.NewConfig()
-	if region := os.Getenv(EnvNameRegion); region != "" {
-		cfg.Region = aws.String(region)
-	}
-	if region := os.Getenv(EnvNameRegion); region != "" {
+	if endpoint := os.Getenv(envNameEndpoint); endpoint != "" {
 		cfg.Endpoint = aws.String(endpoint)
 	}
-	var awsBucket = EnvValueBucket
-	if bucket := os.Getenv(EnvNameBucket); busket != "" {
-		awsBucket = aws.String(bucket)
+	if region := os.Getenv(envNameRegion); region != "" {
+		cfg.Region = aws.String(region)
 	}
-	var awsPath = EnvValuePath
-	if path := os.Getenv(EnvNamePath); path != "" {
-		awsPath = aws.String(path)
+	var awsBucket = envValueBucket
+	if bucket := os.Getenv(envNameBucket); bucket != "" {
+		awsBucket = bucket
+	}
+	var awsPath = envValuePath
+	if path := os.Getenv(envNamePath); path != "" {
+		awsPath = path
 	}
 	sess, err := session.NewSession(cfg)
 	if err != nil {
 		panic(err)
 	}
 	svc := s3.New(sess)
-	store := &S3Storage{
-		bucket: awsBucket,
-		svc:    svc,
+	store := &magicstorage.S3Storage{
+		Bucket: aws.String(awsBucket),
+		Svc:    svc,
 		Path:   awsPath,
 	}
 	return store
